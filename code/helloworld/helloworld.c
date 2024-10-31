7c5ca87afd4486da82e7a01928044d9679c89342
cjsindt <connor.sindt@gmail.com>
2024-10-31 00:48:21 +0000

    naming stuff

---
+++ code/helloworld/helloworld.c
@@ -0,0 +1,68 @@
+/* ------------------------------------------------------------
+    helloworld.c
+------------------------------------------------------------ */
+
+#include <libdragon.h>
+#include "../../core.h"
+#include "../../minigame.h"
+
+/* ---------- MACROS ---------- */
+#define MAZE_SIZE 16    // maze is a square of length MAZE_SIZE
+
+const MinigameDef minigame_def = {
+    .gamename = "Example Game",
+    .developername = "Your Name",
+    .description = "This is an example game.",
+    .instructions = "Press A to win."
+};
+
+/*==============================
+    generate_maze
+    Maze generation function
+==============================*/
+void generate_maze()
+{
+
+    int maze[MAZE_SIZE][MAZE_SIZE] = {0}; 
+
+}
+
+/*==============================
+    minigame_init
+    The minigame initialization function
+==============================*/
+void minigame_init()
+{
+
+}
+
+/*==============================
+    minigame_fixedloop
+    Code that is called every loop, at a fixed delta time.
+    Use this function for stuff where a fixed delta time is 
+    important, like physics.
+    @param  The fixed delta time for this tick
+==============================*/
+void minigame_fixedloop(float deltatime)
+{
+
+}
+
+/*==============================
+    minigame_loop
+    Code that is called every loop.
+    @param  The delta time for this tick
+==============================*/
+void minigame_loop(float deltatime)
+{
+
+}
+
+/*==============================
+    minigame_cleanup
+    Clean up any memory used by your game just before it ends.
+==============================*/
+void minigame_cleanup()
+{
+
+}
