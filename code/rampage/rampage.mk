85e9f7ce090a72b8c431d83d2aa8f36a4278f68b
James Lambert <lambertjamesd@gmail.com>
2024-11-03 22:56:47 -0700

    Fix blender export script

--- code/rampage/rampage.mk
+++ code/rampage/rampage.mk
@@ -3,6 +3,6 @@ ASSETS_LIST += \
 	filesystem/rampage/building.t3dm \
 	filesystem/rampage/player.t3dm
 
-assets/%.t3dm: assets/%.blend tools/mesh_export.py
+assets/rampage/%.glb: assets/rampage/%.blend tools/mesh_export.py
 	@mkdir -p $(@D)
 	$(BLENDER_4) $< --background --python tools/mesh_export.py -- $(<:%.blend=%.glb)
--- tools/mesh_export.py
+++ tools/mesh_export.py
@@ -4,4 +4,4 @@ import sys
 print("Blender export scene in GLTF Format in file "+sys.argv[-1])
 
 # Doc can be found here: https://docs.blender.org/api/current/bpy.ops.export_scene.html
-bpy.ops.export_scene.gltf(filepath=sys.argv[-1])
+bpy.ops.export_scene.gltf(filepath=sys.argv[-1], export_extras=True)
