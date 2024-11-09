82ca5683a0d99aab9326abfbb1afed58a6cc2266
jhall.develop <jhall.develop@gmail.com>
2024-11-09 15:23:01 +1000

    EDIT: moved existing game files to seperate folder not to be compiled

---
+++ ignoreGames/examplegame/examplegame.h
@@ -0,0 +1,9 @@
+#ifndef GAMEJAM2024_EXAMPLEGAME_H
+#define GAMEJAM2024_EXAMPLEGAME_H 
+
+    void examplegame_init();
+    void examplegame_fixedloop(float deltatime);
+    void examplegame_loop(float deltatime);
+    void examplegame_cleanup();
+
+#endif
