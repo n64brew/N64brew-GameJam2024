294c93d46b6b6fb999e724ec20869363f2183031
RiPpEr The Grey Fox <ripper253@gmail.com>
2024-11-10 17:30:28 +1100

    Added own assets built from scratch in blender (map and test actor)
    Converted actor to skinned mesh system (animations still not exporting from blender properly)
    
    Added disable_aa_fix to textparms
    
    Converted actor movement code to more advanced version based on example

--- code/rippergame/rippergame.mk
+++ code/rippergame/rippergame.mk
@@ -1,5 +1,8 @@
 
 ASSETS_LIST += \
 	filesystem/rippergame/map.t3dm \
+	filesystem/rippergame/testActor.t3dm \
 	filesystem/rippergame/sand12.ci4.sprite \
-	filesystem/rippergame/stone.ci4.sprite
+	filesystem/rippergame/stone.ci4.sprite \
+	filesystem/rippergame/test32.ci4.sprite \
+	filesystem/rippergame/test32Inverted.ci4.sprite
