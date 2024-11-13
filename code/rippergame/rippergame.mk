1858f33604b830cc472acb1a8c25c5c0dd526271
RiPpEr The Grey Fox <ripper253@gmail.com>
2024-11-13 17:21:44 +1100

    Added team definitions to players, added some basic AI to be replaced later, individual start positions and team sorting during player_init
    objective_data structs and objective entities added, with init/cleanup/draw functions
    
    Updated font drawing functions with disable_aa_fix, added Player 1 position readout to debug text
    
    added detection of real players vs AI
    
    reordered rendering to ensure transparencies work as intended

--- code/rippergame/rippergame.mk
+++ code/rippergame/rippergame.mk
@@ -2,6 +2,9 @@
 ASSETS_LIST += \
 	filesystem/rippergame/map.t3dm \
 	filesystem/rippergame/testActor.t3dm \
+	filesystem/rippergame/testActorGuard.t3dm \
+	filesystem/rippergame/testObjective.t3dm \
+	filesystem/rippergame/objectiveGradient.ia8.sprite \
 	filesystem/rippergame/sand12.ci4.sprite \
 	filesystem/rippergame/stone.ci4.sprite \
 	filesystem/rippergame/test32.ci4.sprite \
