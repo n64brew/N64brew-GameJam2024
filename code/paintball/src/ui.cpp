1513d5e77d7fd3a3a0d3c22f376a46b0012d24d7
Ali Naci Erdem <anacierdem@gmail.com>
2024-11-15 21:52:48 +0300

    Add go text

--- code/paintball/src/ui.cpp
+++ code/paintball/src/ui.cpp
@@ -16,11 +16,13 @@ void UIRenderer::render(const GameState &state)
     rdpq_sync_tile();
     rdpq_sync_pipe(); // Hardware crashes otherwise
 
+    rdpq_textparms_t textparms = { .width = ScreenWidth, .height = ScreenHeight, .align = ALIGN_CENTER, .valign = VALIGN_CENTER };
     if (state.isCountdown) {
-        rdpq_textparms_t textparms = { .width = ScreenWidth, .height = ScreenHeight, .align = ALIGN_CENTER, .valign = VALIGN_CENTER };
         rdpq_text_printf(&textparms, BigFont, 0, 0, "%d", (int)ceilf(3.f - state.gameTime));
 
         rdpq_textparms_t textparms2 = { .width = ScreenWidth, .height = ScreenHeight / 2, .align = ALIGN_CENTER, .valign = VALIGN_CENTER };
         rdpq_text_printf(&textparms2, MainFont, 0, ScreenHeight / 2, "Prepare to paint!");
+    } else if(state.gameTime < 3.5f){
+        rdpq_text_printf(&textparms, BigFont, 0, 0, "Go!");
     }
 }
