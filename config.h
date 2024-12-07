76c640be742b1232771b3a05338400d73b27593c
Flávio <flaviozavan@gmail.com>
2024-12-07 11:13:46 +0200

    Revert config.h changes

--- config.h
+++ config.h
@@ -8,21 +8,21 @@
     ================================================================================================================== */
 
     // Skip asking the number of players and assume PLAYER_COUNT
-    #define SKIP_PLAYERSELECTION  1
+    #define SKIP_PLAYERSELECTION  0
 
     // The number of human players
     #define PLAYER_COUNT  1
 
     // Skip asking the AI difficulty and assume AI_DIFFICULTY
-    #define SKIP_DIFFICULTYSELECTION  1
+    #define SKIP_DIFFICULTYSELECTION  0
 
     // The difficulty of the AI. 
     #define AI_DIFFICULTY  DIFF_MEDIUM
 
     // Skip the minigame selection and boot straight into MINIGAME_TO_TEST
-    #define SKIP_MINIGAMESELECTION  1
+    #define SKIP_MINIGAMESELECTION  0
 
     // The current minigame you want to test
-    #define MINIGAME_TO_TEST  "avanto"
+    #define MINIGAME_TO_TEST  "examplegame"
 
 #endif
