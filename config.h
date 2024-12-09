18eee1f55b47a2fcf5c91d0f0ca2721e988b6e83
jhall.develop <jhall.develop@gmail.com>
2024-12-09 14:05:20 +1000

    EDIT: updated the config

--- config.h
+++ config.h
@@ -14,15 +14,15 @@
     #define PLAYER_COUNT  1
 
     // Skip asking the AI difficulty and assume AI_DIFFICULTY
-    #define SKIP_DIFFICULTYSELECTION  0
+    #define SKIP_DIFFICULTYSELECTION  1
 
     // The difficulty of the AI. 
     #define AI_DIFFICULTY  DIFF_MEDIUM
 
     // Skip the minigame selection and boot straight into MINIGAME_TO_TEST
-    #define SKIP_MINIGAMESELECTION  0
+    #define SKIP_MINIGAMESELECTION  1
 
     // The current minigame you want to test
-    #define MINIGAME_TO_TEST  "examplegame"
+    #define MINIGAME_TO_TEST  "old_gods"
 
 #endif
