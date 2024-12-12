de84e2cb2d20d5edd4792a0b1b37eb7f9d7d6f6b
jhall.develop <jhall.develop@gmail.com>
2024-12-12 22:30:33 +1000

    EDIT: removed un-used assets

--- code/old_gods/old_gods.mk
+++ code/old_gods/old_gods.mk
@@ -1,8 +1,4 @@
 ASSETS_LIST += \
-	filesystem/old_gods/crate00.ci8.sprite \
-	filesystem/old_gods/sand12.ci4.sprite \
-	filesystem/old_gods/checker.sprite \
-	filesystem/old_gods/shadow.i8.sprite \
 	filesystem/old_gods/sea_foam.sprite \
 	filesystem/old_gods/particle.sprite \
 	filesystem/old_gods/player1_panel_64_64.sprite \
@@ -12,8 +8,6 @@ ASSETS_LIST += \
 	filesystem/old_gods/panel_pink_64_64.sprite \
 	filesystem/old_gods/panel_white_64_64.sprite \
 	filesystem/old_gods/white.sprite \
-	filesystem/old_gods/stone.ci4.sprite \
-	filesystem/old_gods/pattern.i8.sprite \
 	filesystem/old_gods/smoke.sprite \
 	filesystem/old_gods/grave.sprite \
 	filesystem/old_gods/pentogram.sprite \
@@ -21,21 +15,19 @@ ASSETS_LIST += \
 	filesystem/old_gods/Jumpman_H1.font64 \
 	filesystem/old_gods/Jumpman_H2.font64 \
 	filesystem/old_gods/Jumpman_P.font64 \
-	filesystem/old_gods/box.t3dm \
-	filesystem/old_gods/cylinder.t3dm \
-	filesystem/old_gods/food.t3dm \
 	filesystem/old_gods/sea_foam.t3dm \
 	filesystem/old_gods/sea_trail.t3dm \
 	filesystem/old_gods/shadow.t3dm \
 	filesystem/old_gods/map2.t3dm \
 	filesystem/old_gods/snake.t3dm \
 	filesystem/old_gods/rat.t3dm \
-	filesystem/old_gods/torus.t3dm \
 	filesystem/old_gods/shark.t3dm \
 	filesystem/old_gods/attackWave.t3dm \
 	filesystem/old_gods/smoke.t3dm \
 	filesystem/old_gods/alter.t3dm \
 	filesystem/old_gods/grave.t3dm \
+	filesystem/old_gods/torus.t3dm \
+	filesystem/old_gods/box.t3dm \
 	filesystem/old_gods/bottled_bubbles.xm64 \
 	filesystem/old_gods/Item2A.wav64 \
 	filesystem/old_gods/sandy_seaside.wav64 \
@@ -53,10 +45,18 @@ DEBUGFILES = $(UNFLLOADER_DIR)debug.c $(UNFLLOADER_DIR)usb.c
 CFLAGS += -Icode/old_gods/AF_Math/include -Icode/old_gods/AF_Lib/include -I$(UNFLLOADER_DIR)
 
 #==== CREDITS =====
-#pikcup rat https://opengameart.org/content/paper-crush-sounds
-# god eat sound effect https://opengameart.org/content/7-assorted-sound-effects-menu-level-up
-#ui and othe effect sounds https://opengameart.org/content/ui-and-item-sounds-sample-1
-# jumpman font https://fontmeme.com/fonts/jumpman-font/
-# Palm Tree https://sketchfab.com/3d-models/palm-tree-e7677eb753ce4c16bc3c5dbaa8aefa27
-# tune https://opengameart.org/content/sandy-seaside-2
-# sound effects https://opengameart.org/content/ui-and-item-sound-effect-jingles-sample-2
+# SOUNDS
+# pick cup rat Credits: Vinrax (CC-BY 3.0) https://opengameart.org/content/paper-crush-sounds
+# god eat sound effect Credits: Joth (CCO) https://opengameart.org/content/7-assorted-sound-effects-menu-level-up
+# Sandy Seaside Credits: (CCO) Spring Spring: https://opengameart.org/content/sandy-seaside-2
+# sound effects Credits: ViRiX Dreamcore (CC-BY 3.0): https://opengameart.org/content/ui-and-item-sound-effect-jingles-sample-2
+# ui and othe effect sounds Credits: ViRiX Dreamcore (CC-BY 3.0) (David McKee) https://opengameart.org/content/ui-and-item-sounds-sample-1
+
+# FONT
+# jumpman font Credits: Pixel Sagas https://fontmeme.com/fonts/jumpman-font/
+# zen dots Credits: Yoshimichi Ohira (SIL OPEN FONT LICENSE Version 1.1) https://fonts.google.com/specimen/Zen+Dots/license
+
+# 3D Models
+# Palm Tree Model Credits: abdulraffay1433 (CC) https://sketchfab.com/3d-models/palm-tree-e7677eb753ce4c16bc3c5dbaa8aefa27
+# Snek, Rat  Model Credits: Quaternius (CC0) https://quaternius.com/packs/easyenemy.html
+
