4bb994c5d9953babbced7b6d60fcb50bbac05403
RiPpEr The Grey Fox <ripper253@gmail.com>
2024-12-14 06:04:27 +1100

    removed test and unneeded files from the asset list to lower the ROM usage of the game

--- code/larcenygame/larcenygame.mk
+++ code/larcenygame/larcenygame.mk
@@ -8,17 +8,12 @@ ASSETS_LIST += \
 	filesystem/larcenygame/ringObjective.t3dm \
 	filesystem/larcenygame/diamondObjective.t3dm \
 	filesystem/larcenygame/theKilogramObjective.t3dm \
-	filesystem/larcenygame/testActor.t3dm \
-	filesystem/larcenygame/testActorGuard.t3dm \
-	filesystem/larcenygame/testObjective.t3dm \
 	filesystem/larcenygame/guardStunHit.wav64 \
 	filesystem/larcenygame/guardSwing.wav64 \
 	filesystem/larcenygame/thiefCaught.wav64 \
 	filesystem/larcenygame/thiefJump.wav64 \
 	filesystem/larcenygame/objectiveTouch.wav64 \
 	filesystem/larcenygame/objectiveGradient.ia8.sprite \
-	filesystem/larcenygame/sand12.ci4.sprite \
-	filesystem/larcenygame/stone.ci4.sprite \
 	filesystem/larcenygame/test32.ci4.sprite \
 	filesystem/larcenygame/test32Inverted.ci4.sprite \
 	filesystem/larcenygame/thiefEye.ci4.sprite \
