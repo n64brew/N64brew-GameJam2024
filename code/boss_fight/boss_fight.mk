e6095f3241f6de6e217a296d6a599a6e78154bc0
Max Bebök <beboek.max@gmail.com>
2024-12-15 12:08:57 +0100

    fix: makefile

--- code/boss_fight/boss_fight.mk
+++ code/boss_fight/boss_fight.mk
@@ -1,24 +1,24 @@
 build/code/boss_fight/%.o: N64_CXXFLAGS += -std=gnu++20 -fno-exceptions -O2
 
-assets_coll = $(wildcard assets/boss_fight/*.coll)
-assets_scene = $(wildcard assets/boss_fight/*.scene)
+BOSS_FIGHT_assets_coll = $(wildcard assets/boss_fight/*.coll)
+BOSS_FIGHT_assets_scene = $(wildcard assets/boss_fight/*.scene)
 
-assets_png = $(wildcard assets/boss_fight/*.png) $(wildcard assets/boss_fight/grass/*.png) \
+BOSS_FIGHT_assets_png = $(wildcard assets/boss_fight/*.png) $(wildcard assets/boss_fight/grass/*.png) \
 	$(wildcard assets/boss_fight/ui/*.png) $(wildcard assets/boss_fight/ptx/*.png) \
 	$(wildcard assets/boss_fight/obj/*.png)
 
-assets_glb = $(wildcard assets/boss_fight/*.glb) $(wildcard assets/boss_fight/obj/*.glb)
-assets_ttf = $(wildcard assets/boss_fight/*.ttf)
-assets_mp3 = $(wildcard assets/boss_fight/bgm/*.mp3)
-assets_wav = $(wildcard assets/boss_fight/sfx/*.wav)
+BOSS_FIGHT_assets_glb = $(wildcard assets/boss_fight/*.glb) $(wildcard assets/boss_fight/obj/*.glb)
+BOSS_FIGHT_assets_ttf = $(wildcard assets/boss_fight/*.ttf)
+BOSS_FIGHT_assets_mp3 = $(wildcard assets/boss_fight/bgm/*.mp3)
+BOSS_FIGHT_assets_wav = $(wildcard assets/boss_fight/sfx/*.wav)
 
-assets_conv = $(patsubst assets/%,filesystem/%,$(assets_coll)) \
-              $(patsubst assets/%,filesystem/%,$(assets_scene)) \
-              $(patsubst assets/%,filesystem/%,$(assets_png:%.png=%.sprite)) \
-              $(patsubst assets/%,filesystem/%,$(assets_glb:%.glb=%.t3dm)) \
-              $(patsubst assets/%,filesystem/%,$(assets_ttf:%.ttf=%.font64)) \
-              $(patsubst assets/%,filesystem/%,$(assets_mp3:%.mp3=%.wav64)) \
-              $(patsubst assets/%,filesystem/%,$(assets_wav:%.wav=%.wav64))
+BOSS_FIGHT_assets_conv = $(patsubst assets/%,filesystem/%,$(BOSS_FIGHT_assets_coll)) \
+              			 $(patsubst assets/%,filesystem/%,$(BOSS_FIGHT_assets_scene)) \
+              			 $(patsubst assets/%,filesystem/%,$(BOSS_FIGHT_assets_png:%.png=%.sprite)) \
+              			 $(patsubst assets/%,filesystem/%,$(BOSS_FIGHT_assets_glb:%.glb=%.t3dm)) \
+              			 $(patsubst assets/%,filesystem/%,$(BOSS_FIGHT_assets_ttf:%.ttf=%.font64)) \
+              			 $(patsubst assets/%,filesystem/%,$(BOSS_FIGHT_assets_mp3:%.mp3=%.wav64)) \
+              			 $(patsubst assets/%,filesystem/%,$(BOSS_FIGHT_assets_wav:%.wav=%.wav64))
 
 #assets/boss_fight/map.scene: assets/boss_fight/map.glb
 #	@echo "    [SCENE] $@"
@@ -64,5 +64,5 @@ filesystem/boss_fight/bgm/Main.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resampl
 filesystem/boss_fight/bgm/Boss.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 19000 --wav-mono --wav-compress 1
 filesystem/boss_fight/map.t3dm: T3DM_FLAGS += --bvh
 
-ASSETS_LIST += $(assets_conv)
+ASSETS_LIST += $(BOSS_FIGHT_assets_conv)
 #	filesystem/boss_fight/feylon_reactor.wav64
