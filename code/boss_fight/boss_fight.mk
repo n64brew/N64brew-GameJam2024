cf25ada253e6ad70ec9e941e5aaf70766222c2b2
Max Bebök <beboek.max@gmail.com>
2024-12-15 12:02:57 +0100

    fix: makefile

--- code/boss_fight/boss_fight.mk
+++ code/boss_fight/boss_fight.mk
@@ -1,4 +1,4 @@
-N64_CXXFLAGS += -std=gnu++20 -fno-exceptions -O2
+build/code/boss_fight/%.o: N64_CXXFLAGS += -std=gnu++20 -fno-exceptions -O2
 
 assets_coll = $(wildcard assets/boss_fight/*.coll)
 assets_scene = $(wildcard assets/boss_fight/*.scene)
@@ -38,29 +38,30 @@ filesystem/boss_fight/%.scene: assets/boss_fight/%.scene
 	@echo "    [SCENE] $@"
 	$(N64_BINDIR)/mkasset -c 2 -w 256 -o filesystem/boss_fight "$<"
 
+BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 22050 --wav-mono
+
 filesystem/boss_fight/bgm/%.wav64: assets/boss_fight/bgm/%.mp3
 	@mkdir -p $(dir $@)
 	@echo "    [SFX] $@"
-	$(N64_AUDIOCONV) $(AUDIOCONV_FLAGS) -o $(dir $@) "$<"
+	$(N64_AUDIOCONV) $(BOSS_FIGHT_AUDIOCONV_FLAGS) -o $(dir $@) "$<"
 
 filesystem/boss_fight/sfx/%.wav64: assets/boss_fight/sfx/%.wav
 	@mkdir -p $(dir $@)
 	@echo "    [SFX] $@"
-	$(N64_AUDIOCONV) $(AUDIOCONV_FLAGS) --wav-compress 0 -o $(dir $@) "$<"
+	$(N64_AUDIOCONV) $(BOSS_FIGHT_AUDIOCONV_FLAGS) --wav-compress 0 -o $(dir $@) "$<"
 	# $(N64_BINDIR)/mkasset -c 3 -o $(dir $@) $@
 
-AUDIOCONV_FLAGS = --wav-resample 22050 --wav-mono
 
 # 8000, 16000, 22050, 32000, 44100
-filesystem/boss_fight/sfx/Coin.wav64: AUDIOCONV_FLAGS = --wav-resample 32000 --wav-mono
-filesystem/boss_fight/sfx/Men0.wav64: AUDIOCONV_FLAGS = --wav-resample 32000 --wav-mono
+filesystem/boss_fight/sfx/Coin.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 32000 --wav-mono
+filesystem/boss_fight/sfx/Men0.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 32000 --wav-mono
 
-filesystem/boss_fight/sfx/PotBreak.wav64: AUDIOCONV_FLAGS = --wav-resample 16000 --wav-mono
-filesystem/boss_fight/sfx/VoidOn.wav64: AUDIOCONV_FLAGS = --wav-resample 18000 --wav-mono
-filesystem/boss_fight/sfx/BoxHit.wav64: AUDIOCONV_FLAGS = --wav-resample 18000 --wav-mono
+filesystem/boss_fight/sfx/PotBreak.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 16000 --wav-mono
+filesystem/boss_fight/sfx/VoidOn.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 18000 --wav-mono
+filesystem/boss_fight/sfx/BoxHit.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 18000 --wav-mono
 
-filesystem/boss_fight/bgm/Main.wav64: AUDIOCONV_FLAGS = --wav-resample 24000 --wav-mono --wav-compress 1
-filesystem/boss_fight/bgm/Boss.wav64: AUDIOCONV_FLAGS = --wav-resample 19000 --wav-mono --wav-compress 1
+filesystem/boss_fight/bgm/Main.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 24000 --wav-mono --wav-compress 1
+filesystem/boss_fight/bgm/Boss.wav64: BOSS_FIGHT_AUDIOCONV_FLAGS = --wav-resample 19000 --wav-mono --wav-compress 1
 filesystem/boss_fight/map.t3dm: T3DM_FLAGS += --bvh
 
 ASSETS_LIST += $(assets_conv)
