5c9cf9ae0d950842fe946bc68b052397a1ddc12b
Giovanni Bajo <rasky@develer.com>
2025-05-02 02:03:39 +0200

    mallard: restore 16 bit samples for intro xm64 now that they're compressed

--- code/mallard/mallard.mk
+++ code/mallard/mallard.mk
@@ -43,5 +43,3 @@ filesystem/mallard/HaloDekMedium.font64: $(ASSETS_DIR)/mallard/HaloDekMedium.ttf
 	@mkdir -p $(dir $@)
 	@echo "    [FONT] $@"
 	$(N64_MKFONT) $(MKFONT_FLAGS) --range 30-39 --range 2e-2e --range 50-50 --range 20-20 --range 57-57 --range 49-49 --range 4E-4E --range 53-53 --range 44-44 --range 52-52 --range 41-41 --size 36 --outline 1 -o $(dir $@) "$<"
-
-filesystem/mallard/mallard_intro_music.xm64: AUDIOCONV_FLAGS += --xm-8bit
